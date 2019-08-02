d0d12cccf7addad3fe6e7d47d661d15b972489ed
bcauchon <bcauchon@gmail.com>
2019-08-02 11:09:34 -0700

    add before each to create a project to add volunteers to and change hard-coded project_id to value from created project in order to satisfy foreign key restraint in volunteers table.

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,24 +1,30 @@
 require "spec_helper"
 
 describe Volunteer do
+  before :each do
+    project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+    project.save
+    @project_id = project.id
+  end
+
   describe '#name' do
     it 'returns the name of the volunteer' do
-      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => @project_id, :id => nil})
       expect(test_volunteer.name).to eq 'Jane'
     end
   end
 
   describe '#project_id' do
-    it 'returns the project_id of the volunteer' do
-      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-      expect(test_volunteer.project_id).to eq 1
+    it 'returns the @project_id of the volunteer' do
+      test_volunteer = Volunteer.new({:name => 'Jane', :project_id => @project_id, :id => nil})
+      expect(test_volunteer.project_id).to eq @project_id
     end
   end
 
   describe '#==' do
     it 'checks for equality based on the name of a volunteer' do
-      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
-      volunteer2 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => @project_id, :id => nil})
+      volunteer2 = Volunteer.new({:name => 'Jane', :project_id => @project_id, :id => nil})
       expect(volunteer1 == volunteer2).to eq true
     end
   end
@@ -29,9 +35,9 @@ describe Volunteer do
     end
 
     it 'returns all volunteers' do
-      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => @project_id, :id => nil})
       volunteer1.save
-      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => @project_id, :id => nil})
       volunteer2.save
       expect(Volunteer.all).to eq [volunteer1, volunteer2]
     end
@@ -39,7 +45,7 @@ describe Volunteer do
 
   describe '#save' do
     it 'adds a volunteer to the database' do
-      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => @project_id, :id => nil})
       volunteer1.save
       expect(Volunteer.all).to eq [volunteer1]
     end
@@ -47,9 +53,9 @@ describe Volunteer do
 
   describe '.find' do
     it 'returns a volunteer by id' do
-      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer1 = Volunteer.new({:name => 'Jane', :project_id => @project_id, :id => nil})
       volunteer1.save
-      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => @project_id, :id => nil})
       volunteer2.save
       expect(Volunteer.find(volunteer1.id)).to eq volunteer1
     end
