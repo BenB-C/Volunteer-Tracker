167732c499ca9c229babbf62297b47982e1baf08
bcauchon <bcauchon@gmail.com>
2019-08-02 09:21:50 -0700

    add required specs from https://github.com/epicodus-lessons/volunt33r_track3r

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,14 @@
-require 'project'
-require 'volunteer'
-require 'rspec'
-require 'pg'
-require 'pry'
+require "volunteer"
+require "project"
+require "rspec"
+require "pry"
+require "pg"
 
 DB = PG.connect({:dbname => 'volunteer_tracker_test'})
 
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec("DELETE FROM project *;")
-    DB.exec("DELETE FROM volunteer *;")
+    DB.exec('DELETE FROM volunteers *;')
+    DB.exec('DELETE FROM projects *;')
   end
 end
