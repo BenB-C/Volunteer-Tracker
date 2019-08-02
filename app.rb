710e8b4bf7153529855fbb7ad7ce7b612569ca14
bcauchon <bcauchon@gmail.com>
2019-08-02 12:35:30 -0700

    add paths and view for for homepage, listing all projects, and creating a project.

--- app.rb
+++ app.rb
@@ -6,6 +6,18 @@ require 'pry'
 require 'pg'
 also_reload 'lib/**/*.rb'
 
-get ('/') do
+DB = PG.connect({:dbname => "volunteer_tracker"})
 
+get ('/') do
+  redirect to '/projects'
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/new_project') do
+  Project.new({:title => params[:title]}).save
+  redirect to '/projects'
 end
